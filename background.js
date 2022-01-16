2a9a10242c97322c52c20d60fbcaffab9a6e8cda
Ammar <ammarmahmood288@gmail.com>
2022-01-15 22:22:55 -0500

    fixed syntax

--- background.js
+++ background.js
@@ -31,8 +31,7 @@ function restore_options() {
   });
 }
 document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click',
-    save_options);
+document.getElementById('save').addEventListener('click', save_options);
 
 
 chrome.runtime.onInstalled.addListener(function () {
